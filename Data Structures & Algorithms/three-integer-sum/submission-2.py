52465c2dbe37a24cfb91fa4f0ca6cc623db09664
Mazzone Marco <marco.mazzone06@gmail.com>
2026-09-10 18:56:09 +0200

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Brute force Solution:
+        # 3 nested loops:
+        #[-1, 0, 1, 2, -1, -4]
+        # ^ --> first loop
+        #   ^ --> secondo loop
+        #     ^ --> third loop
+        # Time complexity = O(N^3): we have three different nested loops
+        # Space complexity = O(1) = Costant 
+        # THIS IS NOT THE OPTIMAL SOLUTION
+
+        res = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l = i + 1
+            r = len(nums) - 1
+
+            while l < r:
+                threeSum = nums[i] + nums[l] + nums[r]
+
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        
+        return res
