bfbb926f7381f9c7840a35b4c5188540d4260e47
Mazzone Marco <marco.mazzone06@gmail.com>
2026-09-12 13:23:12 +0200

    Add: find-target-in-rotated-sorted-array - submission-5

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-5.py
@@ -0,0 +1,38 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        # binary search --> time complexity = O(logN)
+        # we can use binary search because the array is sorted in ascending order.
+        # the array could be rotated.
+        # Space complexity = O(1)   = costant
+        
+        #brute force solution:
+        # use a for loop:
+        # time complexcity = O(N) 
+
+        # [3,4,5,6,1,2], target = 1
+        #  ^ --> inf
+        #.           ^ --> sup
+        # trivial case: nums[inf] < nums[sup] --> the array is not rotated or it is rotated N times where N is the number of values
+
+        inf = 0
+        sup = len(nums) - 1
+        mid = (sup + inf) // 2
+
+        while inf <= sup:
+            if nums[mid] == target:
+                return mid
+            
+            if nums[mid] >= nums[inf]:
+                if nums[inf] <= target < nums[mid]:
+                    sup = mid - 1
+                else:
+                    inf = mid + 1
+                mid = (sup + inf) // 2
+            else:
+                if nums[mid] < target <= nums[sup]:
+                    inf = mid + 1
+                else:
+                    sup = mid - 1
+                mid = (sup + inf) // 2
+        return -1 
