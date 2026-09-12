05cf17aa832c3aae423f91f581aeb27e8f487738
Mazzone Marco <marco.mazzone06@gmail.com>
2026-09-12 15:25:47 +0200

    Add: valid-binary-search-tree - submission-4

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-4.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Time complexity = O(N) each node is analyzed only once
+# Space Complexity = O(N) we're using a queue as an additional data structure in order to resolve this problem
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+
+        def isValid(root, left, right):
+
+            if not root:
+                return True
+            
+            if not (left < root.val < right):
+                return False
+            
+            return (isValid(root.left, left, root.val) and isValid(root.right, root.val, right))
+        
+        return isValid(root, float('-inf'), float('inf'))
