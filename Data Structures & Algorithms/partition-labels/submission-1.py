3e51f38e9fc039bf87787aacecdc628fa8cc2554
Mazzone Marco <marco.mazzone06@gmail.com>
2026-09-14 17:21:05 +0200

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.py
@@ -0,0 +1,43 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+
+        hashtable = {}  # lettera --> ultimo indice di apparizione apparizione 
+        for index, char in enumerate(s):
+            hashtable[char] = index
+
+
+
+
+        res = []
+        size = end = 0
+        for index, char in enumerate(s):
+            size += 1
+            end = max(end, hashtable[char])
+
+            if index == end:
+                res.append(size)
+                size = 0
+        
+        return res
+
+
+
+
+
+
+        # hashtable {
+        # x : 3
+        # y : 4
+        # z : 7
+        # b : 9
+        # i : 10
+        # s : 11
+        # l : 12 }
+
+
+  
+
+        
+            
+
+        
